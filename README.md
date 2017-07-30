95c81d225ce3899f094ef7a987fe9f5035a76251
astrosquid <varcap.cb@gmail.com>
2017-07-30 01:24:09 -0400

    Connecting to database now working. Error conditions and messages need work.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # basekeep
-A file driven utility for maintaining your PostgreSQL database through version control.
+🏰 A file driven utility for maintaining your PostgreSQL database through version control.
+
+## Setting up database tracking
+After you've decided on which database you'd like to track, you'll need to make a folder (in the appropriate location within your project's repo) that has the same name as your database. For instance if you had an app with a database called "myappdb", your database folder will also be called "myappdb".
+
+Inside this folder should be a list of important settings, such as users and user permissions. It should also contain folders named after schemas. 
+
+Schema folders will contain JSON files for tables, and should also include a JSON file with the same name having the entry `"is_schema": "true"`.
+
+Create a user for this database named `basekeep` and grant it all permissions. Scary, right? Give it a password. 🔮
+
+In the database folder, please add a file called `.secrets.json`. In it, add an entry `"dbpass": BASEKEEP-PASSWORD`. Don't forget to add this file to your `.gitignore`.
+
+Usage: `python3 basekeep -l <db-folder>`
+
+TODO: Add password flag to program invokation. 
+
+TODO: Undecided on trigger storage.
--- basekeep.py
+++ basekeep.py
@@ -1,9 +1,11 @@
-#!/bin/python
+#!/bin/python3
 
-import json
-from pathlib import Path
-import psycopg2
 import argparse
+import json
+import psycopg2
+import sys
+
+from pathlib import Path
 
 parser = argparse.ArgumentParser(description='Maintain your database structure using directories and files.')
 parser.add_argument("-l", "--location", dest="dblocation", required=True, type=str, help="The top directory of your database. Required.")
@@ -14,10 +16,26 @@ path = Path(dblocation)
 
 dbname = str(Path(dblocation)).split('/')[-1]
 
-confirm = input("Run updates on database " + dbname + "? Some data may be lost.\n")
+confirm = input("Run updates on database \"%s\"? Some data may be lost.\n" % (dbname) ) 
 
 if (confirm == "Yes") or (confirm == "yes") or (confirm == "y"):
     print("Updating " + dbname)
 else:
     print("Aborting.")
+    sys.exit(0)
 
+secretspath = ""
+if (str(path).endswith('/')):
+    secretspath = str(path) + '.secrets.json'
+else:
+    secretspath = str(path) + '/.secrets.json'
+
+with open(secretspath) as secrets_file:
+    secrets = json.load(secrets_file)
+
+dbpassword = secrets["dbpass"]
+
+try:
+    conn = psycopg2.connect("dbname='%s' user='basekeep' host='localhost' password='%s'" % (dbname, dbpassword))
+except:
+    print("Unable to make database connection to %s. Are you sure %s is a basekeep-operable tree?" % (dbname, dbname))
