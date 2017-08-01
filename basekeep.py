1826ceac1ee7292f875a5face2fa5c2ff01499c5
astrosquid <varcap.cb@gmail.com>
2017-07-31 23:19:34 -0400

    Cleanup based on linter suggestions.

--- basekeep.py
+++ basekeep.py
@@ -9,7 +9,6 @@ import sys
 from pathlib import Path
 
 def analyze_schemas(conn, schema_dirs):
-    
     cursor = conn.cursor()
     cursor.execute("""SELECT schema_name FROM information_schema.schemata
         WHERE schema_owner != 'postgres';""")
@@ -20,16 +19,16 @@ def analyze_schemas(conn, schema_dirs):
 
     for schema in schema_dirs:
         if schema not in existing_schemas:
-            print("Addition: " + schema)
+            print "Addition: " + schema
             schema_additions.append(schema)
 
     for schema in existing_schemas:
         if schema not in schema_dirs:
-            print("Deletion: " + str(schema))
+            print "Deletion: " + str(schema)
             schema_removals.append(schema)
 
-    print("Schemata to be added: " + str(schema_additions))
-    print("Schemata to be removed: " + str(schema_removals))
+    print "Schemata to be added: " + str(schema_additions)
+    print "Schemata to be removed: " + str(schema_removals)
 
     confirm = continue_prompt("Is this okay?", "Editing schema.", "Aborting.")
 
@@ -44,49 +43,48 @@ def continue_prompt(question, continue_message, abort_message):
     
     return confirm
 
-parser = argparse.ArgumentParser(description='Maintain your database structure using directories and files.')
-parser.add_argument("-l", "--location", dest="dblocation", required=True, type=str, help="The top directory of your database. Required.")
-args = parser.parse_args()
+def startup():
+    parser = argparse.ArgumentParser(description='Maintain your database structure using directories and files.')
+    parser.add_argument("-l", "--location", dest="dblocation", required=True, type=str, help="The top directory of your database. Required.")
+    args = parser.parse_args()
 
-dblocation = args.dblocation
-path = Path(dblocation)
+    dblocation = args.dblocation
+    path = Path(dblocation)
 
-if os.path.isdir(str(path)) == False:
-    print("Specified directory does not exist or is not directory.")
-    sys.exit(0)
+    if os.path.isdir(str(path)) is not True:
+        print "Specified directory does not exist or is not directory."
+        sys.exit(0)
 
-dbname = str(Path(dblocation)).split('/')[-1]
+    dbname = str(Path(dblocation)).split('/')[-1]
 
-confirm = input("Run updates on database \"%s\"? Some data may be lost.\n" % (dbname) ) 
+    confirm = continue_prompt("Run updates on database \"%s\"? Some data may be lost." % (dbname), "Analyzing file tree for %s." % (dbname), "Aborting.")
+    if confirm is not True:
+        sys.exit(0)
 
-if (confirm == "Yes") or (confirm == "yes") or (confirm == "y"):
-    print("Analyzing file tree for " + dbname)
-else:
-    print("Aborting.")
-    sys.exit(0)
+    basekeep_db_files = next(os.walk(dblocation))[2]
 
-basekeep_db_files = next(os.walk(dblocation))[2]
+    if ("%s.json" % (dbname)) not in basekeep_db_files:
+        print "Aborting, please make sure there's a %s.json file in your db directory. See README." % (dbname)
+    elif ".secrets.json" not in basekeep_db_files:
+        print "Aborting, no secrets file found for specified database. Add .secrets.json to db dir. See README."
 
-if ("%s.json" % (dbname)) not in basekeep_db_files:
-    print("Aborting, please make sure there's a %s.json file in your db directory. See README." % (dbname))
-elif ".secrets.json" not in basekeep_db_files:
-    print("Aborting, no secrets file found for specified database. Add .secrets.json to db dir. See README.")
+    secretspath = ""
+    if str(path).endswith('/'):
+        secretspath = str(path) + '.secrets.json'
+    else:
+        secretspath = str(path) + '/.secrets.json'
 
-secretspath = ""
-if (str(path).endswith('/')):
-    secretspath = str(path) + '.secrets.json'
-else:
-    secretspath = str(path) + '/.secrets.json'
+    with open(secretspath) as secrets_file:
+        secrets = json.load(secrets_file)
+    dbpassword = secrets["dbpass"]
 
-with open(secretspath) as secrets_file:
-    secrets = json.load(secrets_file)
-dbpassword = secrets["dbpass"]
+    schema_dirs = next(os.walk(dblocation))[1]
 
-schema_dirs = next(os.walk(dblocation))[1]
+    try:
+        conn = psycopg2.connect("dbname='%s' user='basekeep' host='localhost' password='%s'" % (dbname, dbpassword))
+        analyze_schemas(conn, schema_dirs)
+    except Exception as exception:
+        print "Connection lost. Stack trace: "
+        print exception
 
-try:
-    conn = psycopg2.connect("dbname='%s' user='basekeep' host='localhost' password='%s'" % (dbname, dbpassword))
-    analyze_schemas(conn, schema_dirs)
-except Exception as e:
-    print("Connection lost. Stack trace: ")
-    print(e)
+startup()
