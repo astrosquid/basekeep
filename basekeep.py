0c13fdb6b06c9353407f6c1d23926c4d3addb007
astrosquid <varcap.cb@gmail.com>
2017-08-06 00:15:37 -0400

    Looking through tables. Needs testing but I'm too tired right now.

--- basekeep.py
+++ basekeep.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#/usr/local/bin/python3
 
 import argparse
 import json
@@ -11,18 +11,20 @@ from pathlib import Path
 
 schema_additions = []
 schema_removals = []
+table_additions = []
+table_removals = []
 
 def analyze_schemas(conn, schema_dirs):
     existing_schemas = get_schemata(conn)
 
     for schema in schema_dirs:
         if schema not in existing_schemas:
-            print("Addition: " + schema)
+            #print("Addition: " + schema)
             schema_additions.append(schema)
 
     for schema in existing_schemas:
         if schema not in schema_dirs:
-            print("Deletion: " + str(schema))
+            #print("Deletion: " + str(schema))
             schema_removals.append(schema)
 
     print("Schemata to be added: " + str(schema_additions))
@@ -34,8 +36,49 @@ def analyze_schemas(conn, schema_dirs):
         sys.exit(0)
 
 def analyze_tables(conn, schema_dirs, dblocation):
+    cur = conn.cursor()
+    file_table_list = []
+    sql = "select table_name from information_schema.tables where table_schema = '{}'"
     for schema in schema_dirs:
-        print("in " + schema + " " + str(next(os.walk(dblocation+"/"+schema))[2]))
+        # this will eventually need to account for triggers.
+        # when the time comes, we can probably use os.path.isdir
+        table_names = next(os.walk(dblocation+"/"+schema))[2]
+        sql = sql.format(schema)
+        cur.execute()
+        db_tables = cur[0]
+        for table in db_tables:
+            if table not in table_names:
+                table_removals.append(table)
+
+        for table in (table_names):
+            if table not in db_tables:
+                table_additions.append(table)
+        #print("in " + schema + " " + str(next(os.walk(dblocation+"/"+schema))[2]))
+        # once we have the table names, we can put them in a list in a tuple
+        # in this tuple, the first value will be the schema name.
+        # we'll have a list of these tuples. e.g.:
+        # [
+            #('character', ['players', 'non-players'])
+            #('item', ['items', 'item_trades'])
+        # ]
+        table_names = [table[:-5] for table in table_names]
+        # should not have '.json' file extensions.
+        #print(str(table_names))
+        file_table_list.append((schema, table_names))
+
+    print("file table list: " + str(file_table_list))
+    # we don't want to raise concerns about tables we're wiping out anyway.
+    # alternatively, we could forgive our query for not finding tables that 
+    # we've already blown away. but that feels underhanded considering the
+    # motivation behind this program.
+    removing_anyway = []
+    for table_tuple in file_table_list:
+        print("analyzing: " + str(table_tuple))
+        if table_tuple[0] in schema_removals:
+            print("looking for " + table_tuple[0])
+            removing_anyway.append(table_tuple)
+
+    print("already removing: " + str(removing_anyway))
 
 def get_schemata(conn):
     # there's most likely a more elegant way of going about this,
